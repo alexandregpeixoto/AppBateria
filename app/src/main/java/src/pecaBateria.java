0ae8a6c0fe9e108a31014ecf3da9066134b097c6
Alexandre Gervasi Peixoto <alexandregpeixoto@gmail.com>
2017-09-18 21:15:59 -0300

    Update 1;

---
+++ app/src/main/java/src/pecaBateria.java
@@ -0,0 +1,36 @@
+package src;
+
+/**
+ * Created by alexa on 18/09/2017.
+ */
+
+public class pecaBateria {
+
+    int id;
+    int notaAtribuida;
+    int portaAnalogica;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNotaAtribuida() {
+        return notaAtribuida;
+    }
+
+    public void setNotaAtribuida(int notaAtribuida) {
+        this.notaAtribuida = notaAtribuida;
+    }
+
+    public int getPortaAnalogica() {
+        return portaAnalogica;
+    }
+
+    public void setPortaAnalogica(int portaAnalogica) {
+        this.portaAnalogica = portaAnalogica;
+    }
+}
